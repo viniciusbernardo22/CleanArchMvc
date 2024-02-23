09fa340465afb76ced2c09a535743f1c781b78bb
Vinicius <vini383@gmail.com>
2024-02-23 16:57:08 -0300

    feat: added domain validation class

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,12 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using CleanArchMvc.Domain.Validation;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+        
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            
+            Id = id;
+            ValidateDomain(name);
+        }
+        
         public ICollection<Product> Products { get; set; }
+
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name, Name is Required");
+            
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, Name is too short and require the minimum of 3 characters");
+
+            Name = name;
+        }
     }
 }
---
+++ CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CleanArchMvc.Domain.Validation
+{
+    public class DomainExceptionValidation : Exception
+    {
+        private DomainExceptionValidation(string error): base(error)
+        {
+            
+        }
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError) throw new DomainExceptionValidation(error);
+        }
+        
+    }
+}
