caefcaacbdd75d79602f73854a75396acd50a8f5
Vinicius <vini383@gmail.com>
2024-02-23 17:43:11 -0300

    feat: added domain interfaces to be implemented outside domain layer

---
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+
+        Task<Category> GetbyId(int id);
+
+        Task<Category> Create(Category category);
+        
+        Task<Category> Update(Category category);
+        
+        Task<Category> Delete(Category category);
+
+    }
+}
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetCategories();
+
+        Task<Product> GetbyIdAsync(int id);
+        
+        Task<Product> GetProductCategoryAsync(int id);
+
+        Task<Product> CreateAsync(Product category);
+        
+        Task<Product> UpdateAsync(Product category);
+        
+        Task<Product> DeleteAsync(Product category);
+
+    }
+}
