d81b1e3dc3cbf2cbac932aaa197d58702e0a6714
Vinicius <vini383@gmail.com>
2024-02-23 16:42:42 -0300

    feat: added domain entities and relations between them

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
