5e8a23ff2f30465b7654441ffa3a521bf9712e42
Vinicius <vini383@gmail.com>
2024-02-26 17:04:32 -0300

    feat: implementing repositories category and product

---
+++ CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+        
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Category> GetbyId(int id) => await _context.Categories.FindAsync(id);
+        
+        public async Task<IEnumerable<Category>> GetCategories() => await _context.Categories.ToListAsync();
+        
+        public async Task<Category> Create(Category category)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+        
+        public async Task<Category> Update(Category category)
+        {
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Delete(Category category)
+        {
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+        
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<Product>> GetProducts() => await _context.Products.ToListAsync();
+        
+        public async Task<Product> GetbyIdAsync(int id) => await _context.Products.FindAsync(id);
+        
+        public async Task<Product> GetProductCategoryAsync(int id)
+        {
+            //eager load
+            return await _context.Products
+                .Include(c => c.Category)
+                .SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _context.Update(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> DeleteAsync(Product product)
+        {
+            _context.Remove(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+    }
+}
