92c081ebdaf7844840c2c5f00f8b9a48f9bebe53
Vinicius <vini383@gmail.com>
2024-02-26 16:05:19 -0300

    config: added base dataconfig on appDbContext

--- CleanArchMvc.Infra.Data/Context/AppDbContext.cs
+++ CleanArchMvc.Infra.Data/Context/AppDbContext.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchMvc.Infra.Data.Context
 {
     public class AppDbContext : DbContext
     {
-        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+         public DbSet<Category> Categories;
+         public DbSet<Product> Products;
+
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+         {
+             base.OnModelCreating(modelBuilder);
+
+             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+         }
     }
 }
