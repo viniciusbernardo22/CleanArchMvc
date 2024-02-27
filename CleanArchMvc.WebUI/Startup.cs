c48c9d11455894ca1892ad5a9c3a5f6884810d45
Vinicius <vini383@gmail.com>
2024-02-27 15:24:53 -0300

    feat: config of services

--- CleanArchMvc.WebUI/Startup.cs
+++ CleanArchMvc.WebUI/Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CleanArchMvc.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +19,7 @@ namespace CleanArchMvc.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure(Configuration);
             services.AddControllersWithViews();
         }
 
